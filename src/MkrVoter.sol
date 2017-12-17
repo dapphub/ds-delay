4ba1f2925ce7aa704916f7508a7df4b1add1f772
Nikolai Mushegian <nikolai@nexusdev.us>
2017-12-17 16:24:40 -0500

    draft mkr-voter

--- src/MkrVoter.sol
+++ src/MkrVoter.sol
@@ -1,4 +1,20 @@
+// Self-contained action staging contract. A good candidate to own a `DSProxy` object.
+
 pragma solidity ^0.4.19;
 
 contract MkrVoter {
+    address public ice;
+    address public cool;
+    address public hot;
+    Action[] public acts;
+
+    struct Action {
+        address whom_box; // target
+        uint256 mana_wad; // eth value
+        bytes   data_fax; // calldata
+        bool    done_fin; // done
+    }
+
+
+    function load(Action a) returns (uint256 actID);
 }
