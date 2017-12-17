7ec82a0282dbdfc91abcc238d24f2f6ac04cf010
Nikolai Mushegian <nikolai@nexusdev.us>
2017-12-17 16:14:36 -0500

    dapp init MkrVoter

---
+++ src/MkrVoter.t.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.19;
+
+import "ds-test/test.sol";
+
+import "./MkrVoter.sol";
+
+contract MkrVoterTest is DSTest {
+    MkrVoter voter;
+
+    function setUp() public {
+        voter = new MkrVoter();
+    }
+
+    function testFail_basic_sanity() public {
+        assertTrue(false);
+    }
+
+    function test_basic_sanity() public {
+        assertTrue(true);
+    }
+}
